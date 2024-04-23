a1605bdd6b1796f2f249ae121de06d0c1db3d88e
lcottner <laceycottner@gmail.com>
2024-04-23 15:06:07 -0400

    Bat Enemy-No Collision
    Added a bat enemy that moves across the screen to the left. Need to implement collision detection to kill the player.

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+
+    
+{
+
+    private int direction = -1;
+    public float movingSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move left
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-movingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
