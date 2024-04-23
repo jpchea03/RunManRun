5ae92c0c5668ae00643bcee3d716eac40fffb3cd
Joseph Cheatham <jpchea03@gmail.com>
2024-04-23 17:32:56 -0400

    Revert "Squashed commit of the following:"
    This reverts commit e4ecd4e45d8b2dd5887535b24cd288959e9bc0b5.

--- Assets/Scripts/EndGameCheck.cs
+++ Assets/Scripts/EndGameCheck.cs
@@ -24,15 +24,4 @@ public class EndGameCheck : MonoBehaviour
         //Load end game scene
         SceneManager.LoadScene("EndGameScene");
     }
-
-    //Collision detection with enemies
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        //Check if the collided object has the tag "enemy" and game over flag is not set
-        if (collision.gameObject.CompareTag("enemy") && !gameOver)
-        {
-            //Trigger end game screen
-            EndGame();
-        }
-    }
 }
--- Assets/Scripts/SpawnEntity.cs
+++ Assets/Scripts/SpawnEntity.cs
@@ -1,53 +1,38 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEntity : MonoBehaviour
 {
-    //Variables and objects
-    public GameObject obstaclePrefab; //the obstacle prefab that is spawned
-    public GameObject enemyPrefab; //The enemy prefab that is spawned on top of obstacles
+    //variables and objects
+    public GameObject obstacle; //the obstacle that is spawned
     public float maxX;
     public float minX;
     public float maxY;
     public float minY;
-    public float timeBetweenObstacleSpawn; //Time between spawning obstacles
-    public float enemyspawnProbability = 0.5f;
+    public float timeBetweenSpawn;
     private float spawnTime;
 
-    //Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        //Checks if enough time has passed between spawns
+        //checks if enough time has passed between spawns
         if (Time.time > spawnTime)
         {
-            SpawnObstacle();
-            spawnTime = Time.time + timeBetweenObstacleSpawn;
+            Spawn();
+            spawnTime = Time.time + timeBetweenSpawn;
         }
     }
 
-    void SpawnObstacle()
+    void Spawn()
     {
         //Picks random x and y values from given range
-        float randomX = UnityEngine.Random.Range(minX, maxX);
-        float randomY = UnityEngine.Random.Range(minY, maxY);
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
 
-        //Instantiate the obstacle at a random position
-        GameObject spawnedObstacle = Instantiate(obstaclePrefab, transform.position + new Vector3(randomX, randomY, 0), Quaternion.identity);
+        GameObject spawnedObstacle = Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
 
-        //Randomly decide whether to spawn an enemy on top of this obstacle
-        if (UnityEngine.Random.value < enemyspawnProbability)
-        {
-            //Adjust the position of the enemy prefab to appear on top of the obstacle along the Y-axis
-            Vector3 enemyPosition = spawnedObstacle.transform.position + Vector3.up * spawnedObstacle.GetComponent<SpriteRenderer>().bounds.size.y / 2;
-            GameObject spawnedEnemy = Instantiate(enemyPrefab, enemyPosition, Quaternion.identity);
-
-            //Destroy the spawned enemy after 10 seconds
-            Destroy(spawnedEnemy, 10f);
-        }
-
-        //Destroy the spawned obstacle after 10 seconds
+        // Destroy the spawned object after 10 seconds
         Destroy(spawnedObstacle, 10f);
     }
 }
