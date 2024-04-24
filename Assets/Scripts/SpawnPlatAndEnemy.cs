8fd6a5faa82cbf7d89ad7cbc9dbd0689840ef8f9
lcottner <laceycottner@gmail.com>
2024-04-23 20:02:46 -0400

    Bat Spawns with Platform
    50% chance for bat to spawn. Another chance for bat to spawn above/below but slightly favors above.

--- Assets/Scripts/SpawnPlatAndEnemy.cs
+++ Assets/Scripts/SpawnPlatAndEnemy.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnEntity : MonoBehaviour
+public class SpawnPlatAndEnemy : MonoBehaviour
 {
     //variables and objects
     public GameObject obstacle; //the obstacle that is spawned
+    public GameObject enemy; //the enemy that is spawned
     public float maxX;
     public float minX;
     public float maxY;
@@ -29,11 +30,27 @@ public class SpawnEntity : MonoBehaviour
         //Picks random x and y values from given range
         float randomX = Random.Range(minX, maxX);
         float randomY = Random.Range(minY, maxY);
-       
+        //Random num = new Random();
+        //num.Next(1,10);
+
+        float ranChance = Random.Range(1, 10);
 
         GameObject spawnedObstacle = Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
+        GameObject spawnedEnemy;
+
+        if (ranChance <= 5)
+        {
+            ranChance = Random.Range(1, 5);
+
+            if (ranChance <= 2)
+                spawnedEnemy = Instantiate(enemy, transform.position + new Vector3(randomX + 10f, randomY + ranChance, 0), transform.rotation);
+            else
+                spawnedEnemy = Instantiate(enemy, transform.position + new Vector3(randomX + 10f, randomY - 1f, 0), transform.rotation);
+        }
+
 
         // Destroy the spawned object after 10 seconds
         Destroy(spawnedObstacle, 10f);
+        Destroy(spawnedObstacle, 15f);
     }
 }
